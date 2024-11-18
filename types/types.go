611a60451f18ad1201cfdc2fdc11e62c1c5518f1
nnxmxni <joshchief169@gmail.com>
2024-11-18 17:52:31 +0100

    feat: Implemented centralized api response struct

--- types/types.go
+++ types/types.go
@@ -1,1 +1,8 @@
 package types
+
+type APIResponseBody struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Token   string      `json:"token,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
