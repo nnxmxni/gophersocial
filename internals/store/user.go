6d0c43b3836f65c297c731cdd9ebdc0bdc26dd1f
nnxmxni <joshchief169@gmail.com>
2024-11-28 20:13:53 +0100

    feat: Set SQL timeouts

--- internals/store/user.go
+++ internals/store/user.go
@@ -25,6 +25,9 @@ func (s UserStore) Create(ctx context.Context, user *User) error {
 		INSERT INTO users (email, password) 
 		VALUES ($1, $2) RETURNING id, created_at
 	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	err := s.db.QueryRowContext(
 		ctx,
 		query,
