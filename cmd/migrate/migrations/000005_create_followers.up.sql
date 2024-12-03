5eb13541cdccc01118f7cea324f6ecc7d7c0e962
nnxmxni <joshchief169@gmail.com>
2024-12-03 18:48:58 +0100

    feat: Use a check constraint to ensure users don't follow themselves

--- cmd/migrate/migrations/000005_create_followers.up.sql
+++ cmd/migrate/migrations/000005_create_followers.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS followers(
 
     PRIMARY KEY (user_id, follower_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT chk_user_not_self_follow CHECK (user_id != follower_id)
 )
