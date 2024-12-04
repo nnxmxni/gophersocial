ffae636c06ae6dc53f66a895d21708014d11e6cd
nnxmxni <joshchief169@gmail.com>
2024-12-04 18:43:34 +0100

    feat: Added endpoints for follow and user feed features

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -54,7 +54,18 @@ func (app *application) mount() http.Handler {
 		})
 
 		r.Route("/users", func(r chi.Router) {
-			r.Get("/{userID}", app.getUserHandler)
+			r.Route("/{userID}", func(r chi.Router) {
+
+				r.Use(app.userContextMiddleware)
+
+				r.Get("/", app.getUserHandler)
+				r.Put("/follow", app.followUserHandler)
+				r.Put("/unfollow", app.unfollowUserHandler)
+			})
+
+			r.Group(func(r chi.Router) {
+				r.Get("/feed", app.getUserFeedHandler)
+			})
 		})
 	})
 
