eb1ee2a46134a2e4dedbdb6dcd1f0e38d1b3e2d9
nnxmxni <joshchief169@gmail.com>
2025-01-17 16:28:59 +0100

    feat: renamed column `password` to `pwd`

--- cmd/migrate/migrations/000002_create_users.up.sql
+++ cmd/migrate/migrations/000002_create_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id bigserial PRIMARY KEY,
     email citext UNIQUE NOT NULL,
-    password bytea NOT NULL,
+    pwd bytea NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 )
